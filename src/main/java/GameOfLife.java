9c6327803d6d9648da6c3485b63e62972e4b7ab2
zstuder25 <zstuder25@gmail.com>
2017-04-17 22:53:46 -0400

    added functionality to loop through states and recalculate

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -3,11 +3,20 @@ import objects.Grid;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * Created by zmans on 4/15/2017.
  */
 public class GameOfLife {
+
+    private static void generateAndPrint(Grid grid){
+        System.out.println("Current Generation:");
+        System.out.println(grid.printCells());
+        grid.nextGeneration();
+        System.out.println("Next Generation:");
+        System.out.println(grid.printCells());
+    }
 
     public static void main(String[] args) throws FileNotFoundException{
         if(args.length > 1 || args.length < 1){
@@ -16,10 +25,19 @@ public class GameOfLife {
         String gridFilePath = args[0];
         File gridFile = new File(gridFilePath);
         Grid grid = new Grid(gridFile);
-        System.out.println("Current Generation:");
-        System.out.println(grid.printCells());
-        grid.nextGeneration();
-        System.out.println("Next Generation:");
-        System.out.println(grid.printCells());
+        generateAndPrint(grid);
+
+        System.out.println("Would you like to run the next generation?: Y/N");
+        Scanner in = new Scanner(System.in);
+        String cin = in.next();
+        while(!cin.equals("N") && !cin.equals("n")){
+            if(cin.equals("Y") || cin.equals("y")){
+                generateAndPrint(grid);
+            }else{
+                System.out.println("Invalid Input: Please Enter only 'Y' or 'N'");
+            }
+            System.out.println("Would you like to run the next generation?: Y/N");
+            cin = in.next();
+        }
     }
 }
